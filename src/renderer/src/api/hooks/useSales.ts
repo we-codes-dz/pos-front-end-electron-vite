cf13e028ed9d678278d57400dca7403e2852b28b
SAGGOU <omarsaggou@gmail.com>
2024-01-24 16:27:47 +0100

    fix useSales type missing

--- src/renderer/src/api/hooks/useSales.ts
+++ src/renderer/src/api/hooks/useSales.ts
@@ -5,7 +5,7 @@ import APIService from '../services/apiService'
 import { useQuery } from '@tanstack/react-query'
 
 const useSales = (axiosInstance: AxiosInstanceOriginal) => {
-  const salesService = new APIService<any>('/sales', axiosInstance)
+  const salesService = new APIService<any, any>('/sales', axiosInstance)
   const data = useQuery({
     queryKey: SALES,
     queryFn: salesService.apiClient.getAll
