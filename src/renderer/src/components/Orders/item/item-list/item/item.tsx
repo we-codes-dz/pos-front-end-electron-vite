764686d963838eb3447e489892f2ffaa4f8c0b87
Mohamed OUBOUABDELLAH <m.oubouabdellah@gmail.com>
2024-01-25 15:14:42 +0100

    Update item.tsx

--- src/renderer/src/components/Orders/item/item-list/item/item.tsx
+++ src/renderer/src/components/Orders/item/item-list/item/item.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { TOrder } from '../../../../../data/tableCommandData'
 import ProductInformation from './product-information'
 import Controller from './controller/controller'
@@ -9,22 +9,21 @@ type TProps = {
   item: TOrder
 }
 const Item = ({ item }: TProps) => {
-  const [quantity, setQuantity] = useState<number>(1)
-  const { updateItemQuantity } = useBoundStore((state) => state)
+  const [quantity, setQuantity] = useState<number>(item.quantity)
+  //   const { updateItemQuantity } = useBoundStore((state) => state)
   const { deleteItemFromCurrentOrder } = useBoundStore((state) => state)
   const addQuantity = () => {
     setQuantity((prev) => prev + 1)
-    updateItemQuantity(item.product.id, quantity)
   }
   const minusQuantity = () => {
     if (quantity > 1) {
       setQuantity((prev) => prev - 1)
-      updateItemQuantity(item.product.id, quantity)
     }
   }
   const deleteItem = () => {
     deleteItemFromCurrentOrder(item.product.id)
   }
+
   return (
     <div className="flex flex-col  rounded-md relative border-2">
       {/*/  //? delete command button*/}
@@ -39,11 +38,7 @@ const Item = ({ item }: TProps) => {
       <ProductInformation item={item} />
       {/* //? total of the item **/}
       <div className="w-ful  flex justify-between pl-2 pb-2">
-        <Controller
-          addQuantity={addQuantity}
-          minusQuantity={minusQuantity}
-          quantity={item.quantity}
-        />
+        <Controller addQuantity={addQuantity} minusQuantity={minusQuantity} quantity={quantity} />
         <Total price={item.price} quantity={quantity} />
       </div>
     </div>
