7cafa85cf35009b5b055dc73f37f508678c7a86f
Mohamed OUBOUABDELLAH <m.oubouabdellah@gmail.com>
2024-01-25 13:43:13 +0100

    Implemented Functionality: Clicking on a Product in Seller Workspace Adds it to Current Orders Item List
     Added the feature where clicking on a product in Seller Workspace results in the addition of the product to the itemList in current order.

--- src/renderer/src/components/Products/item-list.tsx
+++ src/renderer/src/components/Products/item-list.tsx
@@ -1,27 +1,39 @@
-import { TProduct } from "@renderer/types/type-schema";
-import { BASE_URL } from "../../utils/constants";
+import { TItem, TProduct } from '@renderer/types/type-schema'
+import { BASE_URL } from '../../utils/constants'
+import { useBoundStore } from '@renderer/stores/store'
 type TProps = {
-  item: TProduct;
-};
+  item: TProduct
+}
 const ProductItem = ({ item }: TProps) => {
+  const { addProductToCurrentOrder } = useBoundStore((state) => state)
+  const handleClick = () => {
+    console.log('click on item', item)
+    const product: TItem = {
+      product: item,
+      price: item.price,
+      quantity: 1
+    }
+    addProductToCurrentOrder(product)
+  }
   return (
-    <div className="col-span-12 intro-y sm:col-span-4 2xl:col-span-3 bg-white h-60 hover:scale-105">
+    <div
+      className="col-span-12 intro-y sm:col-span-4 2xl:col-span-3 bg-white h-60 hover:scale-105"
+      onClick={handleClick}
+    >
       <div className="relative p-3 rounded-md box zoom-in">
         <div className="">
           <div className="w-full h-full">
             <img
-              alt="Midone Tailwind HTML Admin Template"
+              alt={item.avatar?.name}
               className="rounded-md "
               src={BASE_URL + item.avatar?.url}
             />
           </div>
         </div>
-        <div className="block mt-3 font-medium text-center truncate ">
-          {item.name}
-        </div>
+        <div className="block mt-3 font-medium text-center truncate ">{item.name}</div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProductItem;
+export default ProductItem
