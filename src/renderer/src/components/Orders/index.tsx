5279b64c4efa9e0ef0a7a57e1f6fcd3d8c450076
SAGGOU <omarsaggou@gmail.com>
2024-01-25 19:01:44 +0100

    add no data scenario control

--- src/renderer/src/components/Orders/index.tsx
+++ src/renderer/src/components/Orders/index.tsx
@@ -5,6 +5,7 @@ import { SwiperSlide } from 'swiper/react'
 import { TSlider } from '../Categories'
 import CarouselWrapper from '../carousel/carousel-wrapper'
 import OrderItem from './item/item'
+import NoData from '../no-data/orders/no-data'
 
 const organizeDataIntoSlider = (data: any): TSlider[] => {
   const chunkSize = 6
@@ -23,18 +24,21 @@ const Orders = () => {
   }
 
   const sliderData: TSlider[] = organizeDataIntoSlider(data)
-
   return (
     <div className="h-full w-full ">
-      <CarouselWrapper className="">
-        {sliderData.map((item: any) => (
-          <SwiperSlide key={item.tableNumber}>
-            <div className="h-full  w-full flex flex-col  justify-between">
-              <OrderItem order={item} />
-            </div>
-          </SwiperSlide>
-        ))}
-      </CarouselWrapper>
+      {sliderData.length > 0 ?
+        <CarouselWrapper className="">
+          {sliderData.map((item: any) => (
+            <SwiperSlide key={item.tableNumber}>
+              <div className="h-full  w-full flex flex-col  justify-between">
+                <OrderItem order={item} />
+              </div>
+            </SwiperSlide>
+          ))}
+        </CarouselWrapper>
+        : <NoData content='No Orders' />
+
+      }
     </div>
   )
 }
