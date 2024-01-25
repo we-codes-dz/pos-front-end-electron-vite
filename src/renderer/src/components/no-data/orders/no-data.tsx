939d4ad54c0f8439c74fa45973ee4e81a6d3fb18
SAGGOU <omarsaggou@gmail.com>
2024-01-25 21:49:26 +0100

    add icon to no-data component

--- src/renderer/src/components/no-data/orders/no-data.tsx
+++ src/renderer/src/components/no-data/orders/no-data.tsx
@@ -1,11 +1,14 @@
+import { BsDatabaseExclamation } from "react-icons/bs";
+
 type TProps = {
     content: string
 }
 const NoData =
     ({ content }: TProps) => {
         return (
-            <div className='flex w-full h-full justify-center items-center'>
-                <div className="flex w-full justify-center font-bold text-xl ">
+            <div className='flex w-full h-full justify-center items-center text-primary'>
+                <div className="flex flex-col gap-2 items-center  w-full justify-center font-bold text-xl ">
+                    <BsDatabaseExclamation className="h-12 w-12" />
                     {content}
                 </div>
             </div>
