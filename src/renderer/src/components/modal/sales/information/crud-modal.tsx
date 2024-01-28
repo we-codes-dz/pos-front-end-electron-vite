5cd330f97f5cb52aefb1ff10b8fd7ecbc54937be
Mohamed OUBOUABDELLAH <m.oubouabdellah@gmail.com>
2024-01-28 16:53:40 +0100

    Integrated Orders Data into Orders Table and Added Order Action Information
     Included orders data in the orders table and supplemented it with details about order actions.

--- src/renderer/src/components/modal/sales/information/crud-modal.tsx
+++ src/renderer/src/components/modal/sales/information/crud-modal.tsx
@@ -1,22 +1,24 @@
-import ConfirmationModalWrapper from "../../shared/confirmation-modal-wrapper";
-import ModalBody from "./crud-modal-body";
-
+import ConfirmationModalWrapper from '../../shared/confirmation-modal-wrapper'
+import ModalBody from './crud-modal-body'
 
 interface Props {
-    title: string;
-    modalIsOpened: boolean;
-    modalHandler: () => void;
-    data: any;
+  title: string
+  modalIsOpened: boolean
+  modalHandler: () => void
+  data: any
 }
 
-const SaleInformationModal =
-    ({ title, modalIsOpened, modalHandler, data }: Props) => {
-        return (
-            //TODO: add cancel and remove customized button to delete modal
-            <ConfirmationModalWrapper title={title} modalIsOpened={modalIsOpened} modalHandler={modalHandler}  >
-                <ModalBody modalHandler={modalHandler} btnClassName="btn-error" data={data} />
-            </ConfirmationModalWrapper>
-        )
-    }
+const SaleInformationModal = ({ title, modalIsOpened, modalHandler, data }: Props) => {
+  return (
+    //TODO: add cancel and remove customized button to delete modal
+    <ConfirmationModalWrapper
+      title={title}
+      modalIsOpened={modalIsOpened}
+      modalHandler={modalHandler}
+    >
+      <ModalBody modalHandler={modalHandler} btnClassName="btn-error" data={data} />
+    </ConfirmationModalWrapper>
+  )
+}
 
 export default SaleInformationModal
--- src/renderer/src/pages/orders-management/index.tsx
+++ src/renderer/src/pages/orders-management/index.tsx
@@ -1,55 +1,51 @@
-import useSales from "@renderer/api/hooks/useSales";
-import OrdersTable from "@renderer/components/table/orders/order-table";
-import OrderTabs from "@renderer/components/tabs/orders/order-tabs";
-import useAxiosPrivate from "@renderer/hooks/useAxiosPrivate";
-import { TProductColumn } from "@renderer/types/type-schema";
-
-
+import useOrders from '@renderer/api/hooks/useOrders'
+import OrdersTable from '@renderer/components/table/orders/order-table'
+import OrderTabs from '@renderer/components/tabs/orders/order-tabs'
+import useAxiosPrivate from '@renderer/hooks/useAxiosPrivate'
+import { TProductColumn } from '@renderer/types/type-schema'
 
 const columnHeaders: TProductColumn[] = [
-    {
-        key: "table",
-        label: "Table"
-    },
-    {
-        key: "total",
-        label: "Total"
-    },
-    {
-        key: "createdAt",
-        label: "Date"
-    },
-    {
-        key: "action",
-        label: "Actions"
-    }
-];
+  {
+    key: 'table',
+    label: 'Table'
+  },
+  {
+    key: 'total',
+    label: 'Total'
+  },
+  {
+    key: 'createdAt',
+    label: 'Date'
+  },
+  {
+    key: 'action',
+    label: 'Actions'
+  }
+]
 const OrdersPage = () => {
+  const axiosInstance = useAxiosPrivate()
+  const { data: orders, isLoading } = useOrders(axiosInstance)
 
-    const axiosInstance = useAxiosPrivate();
-    const { data: orders, isLoading } = useSales(axiosInstance);
+  if (isLoading) return null
+  console.log(orders)
 
-    if (isLoading) return null
-    console.log(orders)
+  if (!isLoading && !orders) return <>no data</>
+  const structuringData = (orders: any) => {
+    return orders.data.data
+  }
+  const structuredOrders = structuringData(orders)
+  console.log(structuredOrders)
 
-    if (!isLoading && !orders) return <>no data</>
-    const structuringData =
-        (orders: any) => {
-            return orders.data.data;
-        }
-    const structuredOrders = structuringData(orders)
-    console.log(structuredOrders)
-
-
-    return (
-        <div className="flex flex-col w-full overflow-y-scroll h-full">
-            {structuredOrders &&
-                <OrderTabs
-                    eatInTable={<OrdersTable headers={columnHeaders} orders={structuredOrders} />}
-                    eatOutTable={<OrdersTable headers={columnHeaders} orders={structuredOrders} />}
-                />}
-        </div>
-    )
+  return (
+    <div className="flex flex-col w-full overflow-y-scroll h-full">
+      {structuredOrders && (
+        <OrderTabs
+          eatInTable={<OrdersTable headers={columnHeaders} orders={structuredOrders} />}
+          eatOutTable={<OrdersTable headers={columnHeaders} orders={structuredOrders} />}
+        />
+      )}
+    </div>
+  )
 }
 
 export default OrdersPage
