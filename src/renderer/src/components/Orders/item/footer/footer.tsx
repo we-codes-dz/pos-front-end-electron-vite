a774603471922acf2b5bbeb68532285d99782cc6
SAGGOU <omarsaggou@gmail.com>
2024-01-29 13:54:37 +0100

    add to kitchend

--- src/renderer/src/components/Orders/item/footer/footer.tsx
+++ src/renderer/src/components/Orders/item/footer/footer.tsx
@@ -2,8 +2,9 @@ import PayPendingButton from "./button/pending-button"
 
 const PayButton = () => {
     return (
-        <div className="flex justify-between h-2/3 px-5 mt-2 ">
+        <div className="flex justify-between h-2/3 px-2 mt-2 gap-2">
             <PayPendingButton title="Pay" className="btn-secondary" />
+            <PayPendingButton title="To Kitchen" className="btn-info" />
             <PayPendingButton title="pending" className="btn-warning" />
         </div>
     )
